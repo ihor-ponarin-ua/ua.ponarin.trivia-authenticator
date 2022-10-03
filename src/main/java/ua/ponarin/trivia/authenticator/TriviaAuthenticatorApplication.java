3ee42c547a1b96ca961a8298ffd01141429ae558
Ihor Ponarin <ihor_ponarin@epam.com>
2022-10-03 18:14:54 +0300

    Initial commit

---
+++ src/main/java/ua/ponarin/trivia/authenticator/TriviaAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package ua.ponarin.trivia.authenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaAuthenticatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriviaAuthenticatorApplication.class, args);
+    }
+
+}
